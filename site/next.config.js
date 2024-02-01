f3d84ad9c1ae2ae1a6d0f98fd8396d7ce4dae40c
Silas Xu <1342638994@qq.com>
2024-02-01 20:53:56 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'My kasm registry.',
+    description: 'Myself store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://kreg.esctab.com/',
+    contactUrl: 'https://github.com/xzy103/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
